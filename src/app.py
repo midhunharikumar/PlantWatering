c5bd284ab2473a3bd0de122bb28e500743746eda
Midhun Harikumar <midhunharikumar@outlook.com>
2021-02-27 13:27:27 -0800

    Intendation error

--- src/app.py
+++ src/app.py
@@ -9,36 +9,39 @@ import numpy as np
 import pendulum
 # import pywemo
 
-def water_now():
-	url = "http://10.0.0.102:8000/water_now/"
 
+def water_now():
+    url = "http://10.0.0.102:8000/run_motor/"
     headers = {"accept": "application/json"}
 
-    response = requests.get(url,headers=headers)
+    response = requests.post(url,headers=headers)
     
     return response.json()
 
 
 def get_data():
     url = "http://10.0.0.102:8000/showdata/"
-
     headers = {"accept": "application/json"}
-
     response = requests.get(url,headers=headers)
     return response.json()
 
 
-if st.sidbar.button('Water now'):
-	water_now()
+if st.sidebar.button('Water now'):
+    water_now()
 
 start_date =  st.sidebar.date_input('start_date',datetime.datetime(2020,1,1))
 end_date =  st.sidebar.date_input('end_date',datetime.datetime(2020,1,1))
 
 df = pd.DataFrame(get_data())
-df = df.iloc[-100:]
-#df.date = np.arange(df.shape[0])
+#df = df.iloc[-1000:]
+# df.date = np.arange(df.shape[0])
 df.columns = ['sensor_name','sensor_value','date']
+df['date'] = pd.to_datetime(df['date'],infer_datetime_format=True)
+
+st.write(type(start_date))
+df = df[df['date'] >= np.datetime64(start_date)]
 df['sensor_value'] = df.sensor_value.apply(lambda x: 829 - x)
+df['sensor_value'].ewm(span = 3600).mean()
 st.write(df)
 fig = plt.figure(figsize=(20,10))
 ax = fig.add_axes([0.4, 0.4, 0.8, 0.8]) # main axes
