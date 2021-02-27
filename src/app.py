5280a229d941987413f2bea0aaa3863f25bc0801
Midhun harikumar <midhunharikumar@outlook.com>
2021-02-27 09:52:00 -0800

    Offloading

--- src/app.py
+++ src/app.py
@@ -3,15 +3,17 @@ import json
 import matplotlib.pyplot as plt
 import streamlit as st
 import pandas as pd
-
-
+import matplotlib
+import numpy as np
 def get_data():
     url = "http://10.0.0.102:8000/showdata/"
 
-    headers = {"accept: application/json"}
+    headers = {"accept": "application/json"}
 
     response = requests.get(url,headers=headers)
-    return response.text()
+    return response.json()
 
 
-st.write(get_data())
+df = pd.DataFrame(get_data())
+df.columns = ['sensor_name','sensor_value','date']
+plt.plot(df.sensor_value.to_list()[:10])
